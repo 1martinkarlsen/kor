f1875056598a54f3402a937136c373bf8a8f7825
Martin Karlsen <1martinkarlsen@gmail.com>
2026-05-18 15:41:00 +0200

    Added detekt and modified some rules to work with compose

--- shared/src/commonMain/kotlin/dk/vixo/kor/ui/App.kt
+++ shared/src/commonMain/kotlin/dk/vixo/kor/ui/App.kt
@@ -1,8 +1,7 @@
 package dk.vixo.kor.ui
 
-import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.arkivanov.decompose.extensions.compose.stack.animation.fade
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
